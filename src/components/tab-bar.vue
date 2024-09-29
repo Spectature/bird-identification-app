266e765cbaa30ec5cd042a779ef7ab405454ac22
杜天宇 <17862705909@163.com>
2024-09-29 21:21:33 +0800

    feat: 切换为uni-appx

---
+++ src/components/tab-bar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const tabSwitch = (item, index) => {
+  console.log(item, index);
+  updateValue(index);
+};
+
+// 定义组件的 props
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+// 更新值的方法
+const updateValue = (value) => {
+  emit("update:modelValue", value);
+};
+</script>
+
+<template>
+  <nut-tabbar @tab-switch="tabSwitch">
+    <nut-tabbar-item tab-title="标签" icon="home"></nut-tabbar-item>
+    <nut-tabbar-item tab-title="标签" icon="category"> </nut-tabbar-item>
+    <nut-tabbar-item tab-title="标签" icon="find"></nut-tabbar-item>
+    <nut-tabbar-item tab-title="标签" icon="cart"></nut-tabbar-item>
+    <nut-tabbar-item tab-title="标签" icon="my"></nut-tabbar-item>
+  </nut-tabbar>
+</template>
+
+<style scoped></style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="content">
+    <view class="container"></view>
+    <view class="bottom">
+      <tab-bar></tab-bar>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import TabBar from "@/components/tab-bar.vue";
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
