328a0666390bbf7c5f58d397bce7e9d9d146ae47
杜天宇 <17862705909@163.com>
2024-10-11 17:00:40 +0800

    feat: 新增使用renderjs、postmessage控制iframe内的video停止播放逻辑，重构script为非setup方式（因为renderjs不支持setup模式）

--- src/pages/today-share.vue
+++ src/pages/today-share.vue
@@ -1,81 +1,82 @@
-<script setup lang="ts">
-import { ref } from "vue";
+<script>
 import MediaLoader from "../components/media-loader.vue";
 import TabBar from "../components/tab-bar.vue";
+import { v4 as uuidv4 } from "uuid";
 
-const result = ref([
-  {
-    type: "video",
-    src: "http://vjs.zencdn.net/v/oceans.mp4",
-    content: "0",
+export default {
+  components: {
+    MediaLoader,
+    TabBar,
   },
-  {
-    type: "audio",
-    src: "https://ting8.yymp3.com/new27/liyugang6/6.mp3",
-    content: "1",
-  },
-  {
-    type: "image",
-    src: "https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg",
-    content: "2",
-  },
-  {
-    type: "audio",
-    src: "https://ting8.yymp3.com/new27/jinzhiwen3/1.mp3",
-    content: "3",
-  },
-  {
-    type: "image",
-    src: "https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg",
-    content: "4",
-  },
-  {
-    type: "video",
-    src: "https://media.w3.org/2010/05/sintel/trailer.mp4",
-    content: "5",
-  },
-]);
 
-// const mediaRef = ref();
+  data() {
+    return {
+      result: [
+        {
+          type: "video",
+          src: "http://vjs.zencdn.net/v/oceans.mp4",
+          content: "0",
+        },
+        {
+          type: "audio",
+          src: "https://ting8.yymp3.com/new27/liyugang6/6.mp3",
+          content: "1",
+        },
+        {
+          type: "image",
+          src: "https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg",
+          content: "2",
+        },
+        {
+          type: "audio",
+          src: "https://ting8.yymp3.com/new27/jinzhiwen3/1.mp3",
+          content: "3",
+        },
+        {
+          type: "image",
+          src: "https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg",
+          content: "4",
+        },
+        {
+          type: "video",
+          src: "https://media.w3.org/2010/05/sintel/trailer.mp4",
+          content: "5",
+        },
+      ],
+      stopStatus: false,
+      currentIndex: 0,
+      originIndex: 0,
+    };
+  },
 
-const dealChange = (index: number) => {
-  // console.log(mediaRef.value);
-  // mediaRef.value[index].setAudioStop();
-  console.log(222);
-  setTimeout(() => {
-    uni.pageScrollTo({
-      scrollTop: 3, // 向上滑动到指定的100像素处
-      duration: 0, // 滑动动画的持续时间，单位是毫秒
-      success: () => {
-        console.log("success");
-      },
+  methods: {
+    // 处理切换的函数
+    dealChange(index) {
+      this.originIndex = this.currentIndex;
+      this.currentIndex = index;
+      if (this.result[this.originIndex].type === "video") {
+        console.log("emit stop");
+        this.stopStatus = !this.stopStatus;
+      }
+    },
+
+    // 停止事件
+    // stop() {
+    //   this.stopStatus = !this.stopStatus;
+    // },
+
+    generateShortUUID() {
+      return uuidv4().split("-")[0]; // 取UUID的前一段
+    },
+  },
+
+  mounted() {
+    this.result.forEach((item) => {
+      if (item.type === "video") {
+        item.className = `iframe${this.generateShortUUID()}`;
+      }
     });
-  }, 1000);
-};
-
-// const currentIndex = ref(0);
-// const startY = ref(0);
-//
-// const startTouch = (event) => {
-//   startY.value = event.touches[0].clientY; // 记录起始位置
-// };
-//
-// const moveTouch = (event) => {
-//   const moveY = event.touches[0].clientY; // 获取当前滑动位置
-//   const distance = startY.value - moveY; // 计算滑动距离
-//
-//   // 在滑动距离较大时，判断是否进行切换
-//   if (distance > 50 && currentIndex.value < result.value.length - 1) {
-//     currentIndex.value += 1; // 向下切换
-//     startY.value = moveY; // 更新起始位置
-//   } else if (distance < -50 && currentIndex.value > 0) {
-//     currentIndex.value -= 1; // 向上切换
-//     startY.value = moveY; // 更新起始位置
-//   }
-// };
-
-const endTouch = () => {
-  // 可以在此处执行其他结束逻辑，例如重置某些状态
+  },
 };
 </script>
 
@@ -89,28 +90,13 @@ const endTouch = () => {
       @change="dealChange"
     >
       <nut-swiper-item v-for="(item, index) in result" :key="index">
-        <!--    <view-->
-        <!--      class="swiper-container"-->
-        <!--      @touchstart="startTouch"-->
-        <!--      @touchmove="moveTouch"-->
-        <!--      @touchend="endTouch"-->
-        <!--    >-->
-        <!--      <view-->
-        <!--        class="swiper-wrapper"-->
-        <!--        :style="{ transform: `translateY(-${currentIndex * 10}%)` }"-->
-        <!--      >-->
-        <!--        <view class="swiper-slide" v-for="(item, index) in result" :key="index">-->
-        <!--   -->
-        <!--        </view>-->
-        <!--      </view>-->
-        <!--    </view>-->
-
         <view class="top">
           <view class="top">
             <media-loader
               ref="mediaRef"
               :type="item.type"
               :src="item.src"
+              :video-class="item.className"
             ></media-loader>
           </view>
           <view class="content">
@@ -120,9 +106,27 @@ const endTouch = () => {
       </nut-swiper-item>
     </nut-swiper>
 
+    <view
+      :prop="stopStatus"
+      :change:prop="
+        renderScript.setStop(this.result[this.originIndex].className)
+      "
+    ></view>
     <view class="bottom"> <tab-bar :current-index="2"></tab-bar></view>
   </view>
 </template>
+
+<script module="renderScript" lang="renderjs">
+export default {
+  methods: {
+    setStop(className) {
+       console.log(className)
+       const iframe = document.querySelector(`.${className}`);
+       iframe?.contentWindow?.postMessage('stop','*');
+      }
+    }
+}
+</script>
 
 <style scoped lang="less">
 .container {
@@ -130,39 +134,9 @@ const endTouch = () => {
   height: 100vh;
   flex-direction: column;
   justify-content: space-between;
-  //position: relative;
   .content {
     display: flex;
     justify-content: center;
   }
-  //.bottom {
-  //  position: absolute;
-  //  bottom: 0;
-  //  width: 100%;
-  //  background-color: lightcoral;
-  //  z-index: 10; /* 层级较高，显示在上面 */
-  //}
 }
-
-//.swiper-container {
-//  height: 100vh; /* 设置容器高度 */
-//  overflow: hidden; /* 隐藏溢出部分 */
-//  position: relative;
-//}
-//
-//.swiper-wrapper {
-//  transition: transform 0.3s ease; /* 设置过渡效果 */
-//  display: flex;
-//  flex-direction: column; /* 垂直排列 */
-//}
-//
-//.swiper-slide {
-//  height: 100vh; /* 每个页面占满屏幕 */
-//  display: flex;
-//  justify-content: center;
-//  align-items: center;
-//  font-size: 24px;
-//  background-color: #f0f0f0; /* 背景色 */
-//  border-bottom: 1px solid #ccc; /* 分隔线 */
-//}
 </style>
